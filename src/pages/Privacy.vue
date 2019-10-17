c5272332fc33dd83247a4069d39f0f630d68ad60
dli <davidli012345@gmail.com>
2019-10-16 17:15:50 -0700

    Updating privacy policy

---
+++ src/pages/Privacy.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+ <h1>Privacy Policy</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit... </p>
+</div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Privacy Policy'
+  }
+}
+</script>
+
+
+<style scoped>
+h1 {
+  color: red
+}
+</style>
