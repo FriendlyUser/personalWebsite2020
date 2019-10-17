2a65816c72814f392b6b5980884e62c1ba252299
dli <davidli012345@gmail.com>
2019-10-16 17:46:08 -0700

    deploying

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -1,7 +1,15 @@
 <template>
 <div>
  <h1>Privacy Policy</h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit... </p>
+  <p>
+Effective Date: October 16, 2019
+
+By installing and launching GFleet mobile applications or games, you agree and consent to all terms and conditions specified in this Privacy Policy.
+
+This Privacy Policy explains how information about you is collected, used, and disclosed by GFleet LLC and any parent or subsidiary (collectively, “GFleet,” “our”, “us” or “we”) when you access or use GFleet websites, mobile applications, and other products and services (collectively, the “Services”) or otherwise interact with us.  This policy applies to GFleet brands, websites, games, applications, and other technologies (we collectively refer to these as “Services”).
+
+Advertising is a core part of GFleet business. Many of our free mobile games, mobile applications and technologies are supported by the ads we and others display. We provide a variety of online advertising services to other companies that want to place ads on our Services.  We display ads, also known as graphical ads, which appear as images, animation, or video. GFleet advertising platforms provide the means for advertisers, agencies, publishers, and ad networks to manage their advertising business. Advertising networks may collect and use data they receive about you to help show ads for products or services that you are more likely to be interested in to make the advertising more useful and effective. By installing and launching our games you understand and consent to our use of data as defined in our Policy Policy.
+  </p>
 </div>
 </template>
 
